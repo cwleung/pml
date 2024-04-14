a81d210e642edbf90a3b9d35b1cc8309f2035a29
Derek Leung <cwleung@duck.com>
2024-04-15 00:11:54 +0100

    base model

--- models/naive_bayes.py
+++ models/naive_bayes.py
@@ -1,6 +1,5 @@
-import numpy as np
-
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -82,52 +81,52 @@ class NaiveBayes(SupBaseModel):
     def score(self, X, y):
         return np.mean(self.predict(X) == y)
 
-        def plot_decision_boundary(self, X, y, resolution=0.01):
-            """
-            Plots the decision boundary for a 2D dataset.
+    def plot_decision_boundary(self, X, y, resolution=0.01):
+        """
+        Plots the decision boundary for a 2D dataset.
 
-            Parameters:
-            X (numpy.ndarray): Input data (n_samples, n_features)
-            y (numpy.ndarray): Target labels (n_samples,)
-            features (list): The indices of the features to be used for the plot (default: [0, 1])
-            resolution (float): The resolution of the meshgrid for plotting (default: 0.01)
+        Parameters:
+        X (numpy.ndarray): Input data (n_samples, n_features)
+        y (numpy.ndarray): Target labels (n_samples,)
+        features (list): The indices of the features to be used for the plot (default: [0, 1])
+        resolution (float): The resolution of the meshgrid for plotting (default: 0.01)
 
-            Returns:
-            None
-            """
-            if X.shape[1] < 2:
-                raise ValueError("plot_decision_boundary only works for 2D datasets.")
-            if X.shape[1] > 2:
-                from sklearn.manifold import TSNE
-                X = TSNE(n_components=2).fit_transform(X)
-                print("Finished t-SNE..., shape: ", X.shape)
+        Returns:
+        None
+        """
+        if X.shape[1] < 2:
+            raise ValueError("plot_decision_boundary only works for 2D datasets.")
+        if X.shape[1] > 2:
+            from sklearn.manifold import TSNE
+            X = TSNE(n_components=2).fit_transform(X)
+            print("Finished t-SNE..., shape: ", X.shape)
 
-            markers = ('s', 'x', 'o', '^', 'v')
-            colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-            cmap = plt.cm.RdYlBu
+        markers = ('s', 'x', 'o', '^', 'v')
+        colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+        cmap = plt.cm.RdYlBu
 
-            x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-            x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-            xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                                   np.arange(x2_min, x2_max, resolution))
+        x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+        x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                               np.arange(x2_min, x2_max, resolution))
 
-            Z = self.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-            Z = Z.reshape(xx1.shape)
+        Z = self.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+        Z = Z.reshape(xx1.shape)
 
-            plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
-            plt.xlim(xx1.min(), xx1.max())
-            plt.ylim(xx2.min(), xx2.max())
+        plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+        plt.xlim(xx1.min(), xx1.max())
+        plt.ylim(xx2.min(), xx2.max())
 
-            # Plot class samples
-            for idx, cl in enumerate(np.unique(y)):
-                plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
-                            alpha=0.8, c=colors[idx],
-                            marker=markers[idx], label=cl, edgecolor='black')
+        # Plot class samples
+        for idx, cl in enumerate(np.unique(y)):
+            plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                        alpha=0.8, c=colors[idx],
+                        marker=markers[idx], label=cl, edgecolor='black')
 
-            plt.xlabel('Feature 1')
-            plt.ylabel('Feature 2')
-            plt.legend(loc='upper left')
-            plt.show()
+        plt.xlabel('Feature 1')
+        plt.ylabel('Feature 2')
+        plt.legend(loc='upper left')
+        plt.show()
 
 
 if __name__ == '__main__':
